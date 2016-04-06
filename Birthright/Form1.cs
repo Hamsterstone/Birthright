fed4b2e4b7c3c30954d8207ff3a86989a780aa16
Hadley Shurmer <HS021@ATCLTD.ATC.ORG.NZ>
2016-04-07 11:09:19 +1200

    rrr

--- Birthright/Form1.cs
+++ Birthright/Form1.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 namespace Birthright
 {
     public partial class Form1 : Form
-    {
+    {//
         Database myDatabase = new Database();
         public Form1()
         {
